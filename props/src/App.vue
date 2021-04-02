fbe4b5f2545db04888481099f0e7044c718e4ab9
Eduardo Munoz <gstv.qnd@outlook.com>
2021-04-02 14:49:32 -0300

    :sparkles: Adding component props and refs

---
+++ props/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>VueJS</h1>
+    <Car :power="power" :upPower="upPower" />
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import Car from "./components/Car.vue";
+
+export default {
+  name: "App",
+  components: {
+    Car,
+  },
+  setup() {
+    let power = ref(1010);
+
+    const upPower = () => {
+      console.log("Aumentando poder...");
+      power.value++;
+    };
+
+    return {
+      power,
+      upPower,
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ props/src/components/Car.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h2>Coche - Composition API</h2>
+    <p>Marca: {{ brand }}</p>
+    <p>Modelo: {{ model }}</p>
+    <p>Potencia: {{ power }}</p>
+    <button @click="increasePower">Aumentar</button>
+    <!-- <p>Potencia: {{ power }}</p> -->
+  </div>
+</template>
+
+<script>
+// import { ref } from "vue";
+export default {
+  props: {
+    power: {
+      type: Number,
+      default: 60,
+    },
+    upPower: Function,
+  },
+  setup(props) {
+    const brand = "Audi";
+    const model = "A4";
+
+    console.log("Component props: ", props.power);
+    // let power = ref(40);
+
+    const increasePower = () => {
+      // power.value += newPower;
+      props.upPower();
+    };
+
+    // const downPower = (newPower) => {
+    //   power.value -= newPower;
+    // };
+
+    return {
+      brand,
+      model,
+      increasePower,
+      // power,
+      // upPower,
+      // downPower,
+    };
+  },
+};
+</script>
+
+<style></style>
